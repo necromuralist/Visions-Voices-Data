4b7127e90c53e7f1bd0a486c891549413e7cbdd4
necromuralist <necromuralist@protonmail.com>
2019-07-29 21:25:43 -0700

    initial version of follower

--- files/posts/nature-of-code/nested-follower/follower.js
+++ files/posts/nature-of-code/nested-follower/follower.js
@@ -23,7 +23,11 @@ let nested_follower_sketch = function(p) {
     p.setup = function() {
         this.canvas = p.createCanvas($("#" + nested_parent_div_id).outerWidth(true), 800);
         p.parent = new NoiseWalker(p);
-        p.followers = [new Follower(p, p.parent), new Follower(p, p.parent), new Follower(p, p.parent)];
+        let followers = 10;
+        p.followers = [new Follower(p, p.parent)];
+        for (let step=0; step < followers; step++) {
+            p.followers.push(new Follower(p, p.followers[step]));
+        };
     };
 
     /**
@@ -32,7 +36,7 @@ let nested_follower_sketch = function(p) {
      * This also clears the background.
      */
     p.draw = function() {
-        p.background(255);
+        p.background(255, 255, 255, 50);
         p.parent.update();
         p.followers.forEach(function(follower) {
             follower.update();
@@ -46,59 +50,62 @@ let nested_follower_sketch = function(p) {
  * @param {P5} p
  */
 function NoiseWalker(p) {
+    // Our kinematics vectors
     this.position = p.createVector(p.width/2, p.height/2);
     this.velocity = p.createVector(0, 0);
-    this.weight = p.round(p.random(5, 10));
+    this.acceleration = p.createVector(0, 0);
+
+    // Time for the perlin noise function
     this.time_x = 0;
     this.time_y = 10000;
     this.time_delta = 0.01;
-    this.acceleration = p.createVector(0, 0);
-    this.max_acceleration = 0.001;
+
+    // The limit of how much we'll accelerate
+    this.max_acceleration = 0.05;
 
     /**
      * Updates the walker's position
      */
     this.walk = function() {
         // set the acceleration using perlin noise
-        this.acceleration.x = p.map(p.noise(this.time_x), 0, 1, 0, this.max_acceleration);
-        this.acceleration.y = p.map(p.noise(this.time_y), 0, 1, 0, this.max_acceleration);
-        console.log(this.acceleration)
-        // update the time
-        this.time_x += this.time_delta;
-        this.time_y += this.time_delta;
-       
-        // setMag always produces the same magnitude (but the orientation stays the same)
-        // this.acceleration.setMag(this.magnitude);
-
+        this.acceleration.x = p.map(p.noise(this.time_x), 0, 1,
+                                    -this.max_acceleration,
+                                    this.max_acceleration);
+        this.acceleration.y = p.map(p.noise(this.time_y), 0, 1,
+                                    -this.max_acceleration,
+                                    this.max_acceleration);
+        // Move the walker
         this.velocity = this.velocity.add(this.acceleration);
         this.position = this.position.add(this.velocity);
 
         // keep it within the window
         if (this.position.x < 0)
-            this.position.x = p.width;
+            this.velocity.x = p.abs(this.velocity.x);
         else if (this.position.x > p.width)
-            this.position.x = 0;
+            this.velocity.x = -this.velocity.x;
         if (this.position.y < 0)
-            this.position.y = p.height;
+            this.velocity.y = p.abs(this.velocity.y);
         else if (this.position.y > p.height)
-            this.position.y = 0;
+            this.velocity.y = -this.velocity.y;
+
+        // update the time
+        this.time_x += this.time_delta;
+        this.time_y += this.time_delta;
     };
 
     /**
-     * draws the walker
+     * draws the walker (for debugging)
      */
     this.display = function() {
         p.stroke(0);
-        //p.background(255, 255, 255, 10);
         p.point(this.position.x, this.position.y);
     };
 
     /**
-     * Calls the walk and update functions
+     * Calls the walk function
      */
     this.update = function() {
         this.walk();
-        this.display();
     };
 }
 
@@ -111,19 +118,17 @@ function NoiseWalker(p) {
  */
 function Follower(p, parent) {
     this.parent = parent;
-    this.variance = p.random(5);
+    this.variance = p.random(1, 10);
     this.position = p.createVector(
         this.parent.position.x + p.random(-this.variance, this.variance),
         this.parent.position.y + p.random(-this.variance, this.variance));
     this.velocity = p.createVector(0, 0);
-    this.magnitude = p.random(0.05, 0.09);
 
     // some colors to cycle through
-    this.red = [63, 123, 191, 191, 191];
-    this.green = [63, 63, 63, 63, 63];
-    this.blue = [191, 191, 191, 127, 63];
-    this.colors = this.red.length;
-    this.color = p.round(p.random(this.colors));
+    this.max_diameter = p.round(p.random(5, 200));
+    this.time_x = p.random(100);
+    this.time_y = p.random(10000, 11000);
+    this.time_delta = 0.005;
 
     /**
      * Moves the Follower
@@ -131,9 +136,7 @@ function Follower(p, parent) {
      * sets the acceleration by pointing to the parent's position
      */
     this.walk = function() {
-        let acceleration = p5.Vector.sub(this.parent.position, this.position);
-       
-        // acceleration.setMag(this.magnitude);
+        let acceleration = p5.Vector.sub(this.parent.position, this.position);       
         this.velocity = this.velocity.add(acceleration);
         this.position = this.position.add(this.velocity);
     };
@@ -144,11 +147,25 @@ function Follower(p, parent) {
      * cycles through the colors as we go
      */
     this.display = function() {
-        p.strokeWeight(p.random(this.variance, 2 * this.variance));
-        p.stroke(this.red[this.color], this.green[this.color], this.blue[this.color]);
-        this.color = (this.color + 1) % this.colors;
+        // set our line width
+        p.strokeWeight(p.map(p.noise(this.time_x, this.time_y),
+                             0, 1, this.variance, 2 * this.variance));
+
+        // set our color
+        p.stroke(63, 63, 191);        
+
+        // don't fill the object
         p.noFill();
-        p.ellipse(this.position.x, this.position.y, p.random(10, 45), p.random(10, 45));
+
+        // draw our object with a random diameter
+        p.ellipse(this.position.x, this.position.y,
+                  p.round(p.random(5, this.max_diameter)),
+                  p.round(p.random(5, this.max_diameter)));
+
+        // update the time
+        this.time_x += this.time_delta;
+        this.time_y += this.time_delta;
+
     };
 
     /**
@@ -160,4 +177,5 @@ function Follower(p, parent) {
     };
 }
 
+// create the p5 object and attach it to the div
 new p5(nested_follower_sketch, nested_parent_div_id);
